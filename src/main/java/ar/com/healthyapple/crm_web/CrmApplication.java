2cf198af68c6dfcba721ce6a86f5464752df441c
Lisandro Martinez <lisandromartinez@gmail.com>
2018-09-05 15:33:51 -0300

    Initial commit

---
+++ src/main/java/ar/com/healthyapple/crm_web/CrmApplication.java
@@ -0,0 +1,12 @@
+package ar.com.healthyapple.crm_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmApplication.class, args);
+    }
+}
